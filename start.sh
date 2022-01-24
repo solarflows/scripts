08fd5076cd183869a2f1b643784398f600bc152a
hyy-666 <hyy-666@github.com>
2022-01-25 00:39:13 +0800

    modify

--- start.sh
+++ start.sh
@@ -1,38 +1,19 @@
 #!/bin/bash
-RUNTIMES=0
-s_dir=$(
-  cd "$(dirname "$0")" || exit
-  pwd
-)
+# 定义全局变量
+# $scripts_dir 脚本所在目录
+scripts_dir=$(cd "$(dirname "$0")";pwd)
+# 脚本开始，设置输出文字的颜色
+# \033[32m 绿色
 echo -e "\033[32m Strat $0 \033[0m"
-if [ -f "$s_dir"/tmp/feeds.conf.default ]; then
-  RUNTIMES=1
-elif [[ ! -f $s_dir/tmp/feeds.conf.default ]] && [[ ! -f $s_dir/tmp/feeds.modify ]]; then
-  mkdir -vp "$s_dir"/tmp
-  cat feeds.conf.default >"$s_dir"/tmp/feeds.conf.default
-  echo -e "\033[32m It's The First Time You Run This Script. \033[0m"
-  sleep 1
-  echo -e "\033[32m Start Initialization ! \033[0m"
-  sleep 1
+# 执行openwrt固件自带的升级脚本
+$(pwd)/scripts/feeds update -a
+# 检索安装所有已有插件到编译环境
+$(pwd)/scripts/feeds install -a
+# 上一条指令执行正常则输出以下语句
+if [ $? ]; then
+  echo -e "\033[32m Install Feeds Well \033[0m"
 else
-  echo -e "\033[31m Please Clean Up! \033[0m"
-  exit 0
+  # 异常输出
+  echo -e "\033[31m Install Feeds Fail ! \033[0m"
+  exit 1
 fi
-
-if [ $RUNTIMES -eq 0 ]; then
-  cat "$s_dir"/newfeeds >>feeds.conf.default
-  if [ $? ]; then
-    cat feeds.conf.default >"$s_dir"/tmp/feeds.modify
-    echo -e "\033[32m Add Feeds To Feeds.conf.default Success \033[0m"
-    echo -e "\033[32m Start Update.sh\033[0m"
-    "$s_dir"/update.sh
-  else
-    echo -e "\033[31m Add Feeds To Feeds.conf.default Fail！\033[0m"
-    exit 0
-  fi
-else
-  echo -e "\033[31m Start $0 ！ \033[0m"
-  echo -e "\033[31m Update root file！ \033[0m" && git pull
-  "$s_dir"/update.sh
-fi
-exit 0
