61fb6a9e5ad7a0c9400a8be0e0fe8d933d72eeda
hyy-666 <57535444+hyy-666@users.noreply.github.com>
2022-01-28 14:30:07 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,9 +2,19 @@
 # 定义全局变量
 # $scripts_dir 脚本所在目录
 scripts_dir=$(cd "$(dirname "$0")";pwd)
+# $lede_dir lede所在目录
+lede_dir=$(pwd)
 # 脚本开始，设置输出文字的颜色
 # \033[32m 绿色
 echo -e "\033[32m Strat $0 \033[0m"
+# 更新本身
+echo -e "\033[31m Update scripts file！ \033[0m"
+cd ${scripts_dir}
+git pull
+# 更新lede仓库
+echo -e "\033[31m Update root file！ \033[0m"
+cd ${lede_dir}
+git pull
 # 执行openwrt固件自带的升级脚本
 $(pwd)/scripts/feeds update -a
 # 检索安装所有已有插件到编译环境
