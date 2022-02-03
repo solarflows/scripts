acdc385174b1c9a8a8518f3ad35b8e351954dfec
hyy-666 <57535444+hyy-666@users.noreply.github.com>
2022-02-03 11:56:19 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -12,7 +12,7 @@ if [[ -f $M_conf ]] && [[ -f $M_seed ]]; then
   if [ "$M_conf" -ot "$M_seed" ]; then
     echo -e "\033[32m Config File Exist ,But Out of Day ! \033[0m"
     echo -e "\033[32m Update Config !\033[0m"
-    cat $M_seed >$M_conf
+    cat $M_seed > $M_conf
     make defconfig
   else
     echo -e "\033[32m You Have Update Config By Yourself ! \033[0m"
@@ -22,7 +22,7 @@ elif [[ -f $M_conf ]] && [[ ! -f $M_seed ]]; then
   echo -e "\033[32m Config File Exist ! \033[0m"
   echo -e "\033[32m Start First Compile ! \033[0m"
 elif [[ ! -f $M_conf ]] && [[ -f $M_seed ]]; then
-  cat $M_seed >$M_conf
+  cat $M_seed > $M_conf
   echo -e "\033[32m Copy Exist Config \033[0m"
   make defconfig
 elif [[ ! -f $M_conf ]] && [[ ! -f $M_seed ]]; then
@@ -34,11 +34,6 @@ make download -j8
 echo -e "\033[32m Start Compile Firmware \033[0m"
 sleep 2
 make -j$(($(nproc) + 1)) || make -j1 V=s || echo -e "\033[31m Compile Fail !\033[0m"
-# if [ !$? ]; then
-#   make -j1 V=s
-# else
-#   echo -e "\033[31m Compile Fail !\033[0m"
-# fi
 end=$(date +%s)
-time=$(echo $start $end | awk '{print $2-$1}')
+time=$(echo spend $start $end | awk '{print $2-$1} seconds')
 echo -e "\033[32m $0 done ! Work Time $time \033[0m"
