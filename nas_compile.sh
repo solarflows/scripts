7ee9040885cb5d8bb961410df89c4f75506bcef5
hyy-666 <57535444+hyy-666@users.noreply.github.com>
2022-02-04 23:53:08 +0800

    fix errors

--- nas_compile.sh
+++ nas_compile.sh
@@ -4,14 +4,14 @@
 scripts_dir = $(cd "$(dirname "$0")";pwd)
 # $lede_dir lede所在目录
 lede_dir = $(pwd)
+# 时间
+date =`date +"%Y-%m-%d %H:%M:%S"`
 # 固件输出目录
 firmware_dir = ${scripts_dir}/Firmware_${date}/Nas
 # 复制配置文件
 cp ${scripts_dir}/conf/Nas.buildinfo ${lede_dir}/.config
 # 开始编译
 ${scripts_dir}/compile.sh
-# 时间
-date =`date +"%Y-%m-%d %H:%M:%S"`
 rm -rf $(find ${lede_dir}/bin/targets/ -type d -name "packages")
 mkdir -vp ${firmware_dir}/{firmware,buildinfo}/
 # 移动固件文件
