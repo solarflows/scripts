e084c8cc63372303219c87cccf03e9715a40f569
hyy-666 <57535444+hyy-666@users.noreply.github.com>
2022-02-05 17:14:08 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -20,7 +20,7 @@ echo -e "\033[32m Strat $0 ! \033[0m"
 # 判断配置文件文件大小与存在性
 if [ ! -e $conf ] || test $conf_size -le $std_size
 then
-  echo '\033[32m conf file < std size start defconfig \033[0m'
+  echo -e "\033[32m conf file < std size start defconfig \033[0m"
   rm -rf $lede_dir/tmp
   make defconfig
 fi
@@ -30,7 +30,7 @@ make download -j$threads
 # 删除已编译的文件
 if [ -e $lede_dir/bin ]
 then
-  echo '\033[32m firmware exits detele it ! \033[0m'
+  echo -e "\033[32m firmware exits detele it ! \033[0m"
   rm -rf $lede_dir/bin
   rm -rf $lede_dir/build_dir/target-*/linux-*
 fi
