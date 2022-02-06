711eac95faf30e3b12fcefdf530d4171bc24eb13
hyy-666 <57535444+hyy-666@users.noreply.github.com>
2022-02-06 09:49:54 +0800

    improve

--- router_compile.sh
+++ router_compile.sh
@@ -14,10 +14,10 @@ then
     # 整理文件
     rm -rf $(find $lede_dir/bin/targets/ -type d -name "packages")
     # 创建目录
-    mkdir -vp $lede_dir/Firmware/$date/Router/firmware/
+    mkdir -vp $lede_dir/Firmware/Router/$(date)/firmware/
     # 移动固件文件
     echo '\033[32m moving firmware file \033[0m'
-    mv -f $(find $lede_dir/bin/targets/ -type f -name "*.buildinfo" -o -name "*.gz" -o -name "*.img") $lede_dir/Firmware/$date/Router/firmware/
+    mv -f $(find $lede_dir/bin/targets/ -type f -name "*.buildinfo" -o -name "*.gz" -o -name "*.img") $lede_dir/Firmware/Router/$(date)/firmware/
     # 展示目录
-    tree $lede_dir/Firmware/$date/Router
+    tree $lede_dir/Firmware/Router/$(date)
 fi
