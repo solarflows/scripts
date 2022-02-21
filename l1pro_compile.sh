7ed30b69fd2a5f4a7e7a573ec87fe6a2813354a3
hyy-666 <hyiy666@gmail.com>
2022-02-21 23:39:35 +0800

    add L1Pro support

---
+++ l1pro_compile.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# 脚本所在目录
+scripts_dir=$(cd "$(dirname "$0")";pwd)
+# lede所在目录
+lede_dir=$(pwd)
+# 复制配置文件
+cp $scripts_dir/conf/L1Pro.buildinfo $lede_dir/.config
+# 开始编译
+$scripts_dir/compile.sh
+if [ -e $lede_dir/bin/targets/**/**/sha256sums ]
+then
+    # 时间
+    date=$(date +"%Y-%m-%d_%H:%M:%S")
+    # 整理文件
+    rm -rf $(find $lede_dir/bin/targets/ -type d -name "packages")
+    # 创建目录
+    mkdir -vp $lede_dir/Firmware/L1Pro/$date
+    # 移动固件文件
+    echo -e "\033[32m moving firmware file \033[0m"
+    mv -f $(find $lede_dir/bin/targets/ -type f -name "*.buildinfo" -o -name "sha256sums" -o -name "*.gz" ! -name "*rootfs*" -o -name "*.img" ! -name "*rootfs*") ~/Firmware/L1Pro/$date/
+    # 展示目录
+    tree $lede_dir/Firmware/L1Pro/$date
+fi
