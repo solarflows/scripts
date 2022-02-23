e7f2225d68486552cce0e5c9ed56b7096ec547a2
hyy-666 <hyiy666@gmail.com>
2022-02-24 00:13:58 +0800

    fix

--- catdrive_compile.sh
+++ catdrive_compile.sh
@@ -14,10 +14,10 @@ then
     # 整理文件
     rm -rf $(find $lede_dir/bin/targets/ -type d -name "packages")
     # 创建目录
-    mkdir -vp $lede_dir/Firmware/CatDrive/$date
+    mkdir -vp ~/Firmware/CatDrive/$date
     # 移动固件文件
     echo -e "\033[32m moving firmware file \033[0m"
     mv -f $(find $lede_dir/bin/targets/ -type f -name "*.buildinfo" -o -name "sha256sums" -o -name "*.gz" ! -name "*rootfs*" -o -name "*.img" ! -name "*rootfs*") ~/Firmware/CatDrive/$date/
     # 展示目录
-    tree $lede_dir/Firmware/CatDrive/$date
+    tree ~/Firmware/CatDrive/$date
 fi
