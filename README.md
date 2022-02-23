ad94c2a2b6965e803a0296bb8c62117c5e274286
hyy-666 <57535444+hyy-666@users.noreply.github.com>
2022-02-23 23:57:56 +0800

    move file

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # openwrt_scripts
 OpenWRT build scripts.
+参考了以下项目
+[P3TERX/aria2.conf](https://github.com/P3TERX/aria2.conf)
---
+++ old/compile.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+# 定义全局变量
+# $scripts_dir 脚本所在目录
+scripts_dir=$(cd "$(dirname "$0")";pwd)
+# $lede_dir lede所在目录
+lede_dir=$(pwd)
+# $conf 配置文件绝对地址
+conf=$(pwd)/.config
+# 配置文件大小
+conf_size=`ls -l $conf | awk '{print $5}'`
+# 配置文件大小
+std_size=$((1024*30))
+# 编译线程数
+threads=$(($(nproc) + 1))
+# 开始时间
+startTime=`date +"%Y-%m-%d %H:%M:%S"`
+# 脚本开始，设置输出文字的颜色
+# \033[32m 绿色
+echo -e "\033[32m Strat $0 ! \033[0m"
+# 判断配置文件文件大小与存在性
+if [ ! -e $conf ] || test $conf_size -le $std_size
+then
+  echo -e "\033[32m conf file < std size start defconfig \033[0m"
+  rm -rf $lede_dir/tmp
+  make defconfig
+fi
+# 执行下载依赖包
+echo -e "\033[32m Download Files \033[0m"
+make download -j$threads
+# 删除已编译的文件
+if [ -e $lede_dir/bin ]
+then
+  echo -e "\033[32m firmware exits detele it ! \033[0m"
+  rm -rf $lede_dir/bin
+fi
+# 开始编译固件（包含简单的异常处理）
+echo -e "\033[32m Start Compile Firmware \033[0m"
+make -j$threads
+if [ ! -e $lede_dir/bin/targets/*/*/sha256sums ] && [ ! -e $lede_dir/bin/targets/*/*/*.gz ]
+then
+  rm -rf $lede_dir/build_dir/target-*/linux-*
+  make -j$threads || make -j1 V=s
+  if [ ! -e $lede_dir/bin/targets/**/**/sha256sums ]
+  then
+    echo -e "\033[31m Compile Fail ! \033[0m"
+    exit 1
+  else
+    echo -e "\033[31m Work done ! \033[0m"
+  fi
+else
+  echo -e "\033[31m Work done ! \033[0m"
+fi
+# 结束时间
+endTime=`date +"%Y-%m-%d %H:%M:%S"`
+st=`date -d  "$startTime" +%s`
+et=`date -d  "$endTime" +%s`
+sumTime=$(($et-$st))
+echo -e "\033[31m $startTime ---> $endTime \033[0m"
+echo -e "\033[32m Total time is : $sumTime second. \033[0m"
